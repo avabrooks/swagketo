b68c35d3492f543880e944fcb11c8bf4d992ec6e
avabrooks <72889462+avabrooks@users.noreply.github.com>
2022-04-24 22:29:54 -0700

    reorganization and continued templates

--- src/main/java/com/nighthawk/csa/MainController.java
+++ src/main/java/com/nighthawk/csa/MainController.java
@@ -1,30 +1,20 @@
 package com.nighthawk.csa;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 @Controller
 public class MainController {
 
     @GetMapping("/posts")
-    public String services() {
-        return "/frontend/posts";
+    public String posts() {
+        return "/userpages/posts";
     }
+
+    @GetMapping("/clubs")
+    public String clubs() {
+        return "/userpages/clubs";
+    }
+
 }
+
